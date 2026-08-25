3a75a918c36898a67d8ea6c0450561105a398533
balajiaj23 <balajiaj0423@gmail.com>
2026-08-25 22:48:13 +0530

    Merge migration branches and fix migrate flag in build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate --noinput
+python manage.py migrate --no-input
 python manage.py seed_data || true
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@college.edu', 'admin123')" || true
